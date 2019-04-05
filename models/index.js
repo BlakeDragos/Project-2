9877577a77a28ef8eff7686201b2f73c34cbc204
Olivia Yang <yang4862@umn.edu>
2019-04-05 17:33:30 -0500

    new

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ var path = require("path");
 var Sequelize = require("sequelize");
 var basename = path.basename(module.filename);
 var env = process.env.NODE_ENV || "development";
-var config = require(__dirname + "/../config/config.json")[env];
+var config = require(__dirname + "/../config/config.js")[env];
 var db = {};
 
 if (config.use_env_variable) {
@@ -35,7 +35,6 @@ Object.keys(db).forEach(function(modelName) {
     db[modelName].associate(db);
   }
 });
-
 
 
 
